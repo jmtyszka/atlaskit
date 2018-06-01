e2dcbded282e90a58fa4e33f3255de844f6d7a17
Mike Tyszka <mike.tyszka@gmail.com>
2018-06-01 13:55:53 -0700

    Extend cit2mni.sh to apply warps to T1w, T2w head and brain and prob atlas. Calc MNI-space det atlas from MNI-space prob atlas.

--- cit2mni.sh
+++ cit2mni.sh
@@ -32,15 +32,17 @@
 # 2018 California Institute of Technology.
 
 # Key directories (edit as needed)
-cit_dir="${HOME}/Box/CIT_SubCortical_Atlas/Labeling/reinf_learn_atlas"
+cit_dir="${HOME}/Box/CIT_SubCortical_Atlas/Templates"
 icmb_dir="${HOME}/Box/Atlases/MNI_ICBM152_2009c/mni_icbm152_nlin_asym_09c"
 
 # CIT168 700 um templates and prob atlas
-cit_t1=${cit_dir}/CIT168_T1w_head_700um.nii.gz
-cit_t2=${cit_dir}/CIT168_T2w_head_700um.nii.gz
+cit_t1_brain=${cit_dir}/CIT168_T1w_700um.nii.gz
+cit_t2_brain=${cit_dir}/CIT168_T2w_700um.nii.gz
+cit_t1_head=${cit_dir}/CIT168_T1w_head_700um.nii.gz
+cit_t2_head=${cit_dir}/CIT168_T2w_head_700um.nii.gz
 
 # Reinforcement learning prob atlas
-RL_prob="${HOME}/Box
+cit_prob="${HOME}/Box/CIT_SubCortical_Atlas/Labeling/reinf_learn_atlas/prob_atlas_bilateral.nii.gz"
 
 if [ $# -lt 1 ]
 then
@@ -54,16 +56,17 @@ fi
 
 atlas_space=$1
 
+# MNI152 T1w and T2w whole head templates
 case ${atlas_space} in
     "FSL")
         out_space="MNI152-FSL"
-        mni_t1=${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz
-        mni_t2=""
+        mni_t1_head=${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz
+        mni_t2_head=""
         ;;
     "2009c")
         out_space="MNI152-2009c"
-        mni_t1=${icmb_dir}/mni_icbm152_t1_tal_nlin_asym_09c.nii
-        mni_t2=${icmb_dir}/mni_icbm152_t2_tal_nlin_asym_09c.nii
+        mni_t1_head=${icmb_dir}/mni_icbm152_t1_tal_nlin_asym_09c.nii
+        mni_t2_head=${icmb_dir}/mni_icbm152_t2_tal_nlin_asym_09c.nii
         ;;
     *)
         echo "* Unknown atlas space - exiting"
@@ -83,10 +86,6 @@ cit2mni_warp=${ants_prefix}1Warp.nii.gz
 logfile=${ants_prefix}Warp.log
 echo "ANTs registration output in ${logfile}"
 
-# CIT168 templates in MNI space
-cit2mni_t1=${out_dir}/CIT168to${out_space}_T1w_head.nii.gz
-cit2mni_t2=${out_dir}/CIT168to${out_space}_T2w_head.nii.gz
-
 # ANTs options
 nthreads=8
 ants_opts="-d 3 -n ${nthreads} -t b -o ${ants_prefix}"
@@ -98,11 +97,11 @@ if [ ! -s ${cit2mni_warp} ]; then
     case ${atlas_space} in
         "FSL")
             echo "  FSL : using T1w templates only for warp estimation"
-            antsRegistrationSyN.sh ${ants_opts} -f ${mni_t1} -m ${cit_t1} 2>&1 > ${logfile}
+            antsRegistrationSyN.sh ${ants_opts} -f ${mni_t1_head} -m ${cit_t1_head} 2>&1 > ${logfile}
             ;;
         "2009c")
             echo "  2009c : using both T1w and T2w templates for warp estimation"
-            antsRegistrationSyN.sh ${ants_opts} -f ${mni_t1} -f ${mni_t2} -m ${cit_t1} -m ${cit_t2} 2>&1 > ${logfile}
+            antsRegistrationSyN.sh ${ants_opts} -f ${mni_t1_head} -f ${mni_t2_head} -m ${cit_t1_head} -m ${cit_t2_head} 2>&1 > ${logfile}
             ;;
         *)
             echo "* Unknown atlas space - exiting"
@@ -111,17 +110,41 @@ if [ ! -s ${cit2mni_warp} ]; then
 
 fi
 
-# Apply warp to CIT168 T1w template
-if [ ! -s ${cit2mni_t1} ]; then
-    echo "Warping CIT168 T1 to ${out_space} space"
-    WarpImageMultiTransform 3 ${cit_t1} ${cit2mni_t1} ${cit2mni_warp} ${cit2mni_affine} --use-BSpline
-fi
+# Apply CIT168 to MNI warp to T1w, T2w (brain and head) and prob atlas images
 
-# Apply warp to CIT168 T2w template
-if [ ! -s ${cit2mni_t2} ]; then
-    echo "Warping CIT168 T2 to ${out_space} space"
-    WarpImageMultiTransform 3 ${cit_t2} ${cit2mni_t2} ${cit2mni_warp} ${cit2mni_affine} --use-BSpline
-fi
+echo "Warping CIT168 images to ${out_space} space"
 
-# Apply warp to CIT168 probabilistic atlas
+# CIT168 templates in MNI space
+cit2mni_t1_brain=${out_dir}/CIT168to${out_space}_T1w_brain.nii.gz
+cit2mni_t2_brain=${out_dir}/CIT168to${out_space}_T2w_brain.nii.gz
+cit2mni_t1_head=${out_dir}/CIT168to${out_space}_T1w.nii.gz
+cit2mni_t2_head=${out_dir}/CIT168to${out_space}_T2w.nii.gz
+cit2mni_prob=${out_dir}/CIT168to${out_space}_prob.nii.gz
+cit2mni_det=${out_dir}/CIT168to${out_space}_det.nii.gz
 
+echo "----------------"
+echo "T1w brain"
+WarpImageMultiTransform 3 ${cit_t1_brain} ${cit2mni_t1_brain} -R ${mni_t1_head} ${cit2mni_warp} ${cit2mni_affine} --use-BSpline
+
+echo "----------------"
+echo "T2w brain"
+WarpImageMultiTransform 3 ${cit_t2_brain} ${cit2mni_t2_brain} -R ${mni_t1_head} ${cit2mni_warp} ${cit2mni_affine} --use-BSpline
+
+echo "----------------"
+echo "T1w head"
+WarpImageMultiTransform 3 ${cit_t1_head} ${cit2mni_t1_head} -R ${mni_t1_head} ${cit2mni_warp} ${cit2mni_affine} --use-BSpline
+
+echo "----------------"
+echo "T2w head"
+WarpImageMultiTransform 3 ${cit_t2_head} ${cit2mni_t2_head} -R ${mni_t1_head} ${cit2mni_warp} ${cit2mni_affine} --use-BSpline
+
+echo "----------------"
+echo "Probabilistic labels"
+WarpTimeSeriesImageMultiTransform 4 ${cit_prob} ${cit2mni_prob} -R ${mni_t1_head} ${cit2mni_warp} ${cit2mni_affine}
+
+# Calculate warped deterministic labels from warp probabilistic labels (p >= 0.25)
+echo "----------------"
+echo "Deterministic labels (p > 0.25)"
+prob2det.sh ${cit2mni_prob} ${cit2mni_det} 0.25
+
+echo "Done"
