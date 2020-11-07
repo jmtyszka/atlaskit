f26f397b68a80423fb71dffc820b5bef992470b5
Mike Tyszka <mike.tyszka@gmail.com>
2020-11-06 16:22:01 -0800

    Add prob weighted metric mean script

--- prob_weighted_metric.py
+++ prob_weighted_metric.py
@@ -25,7 +25,7 @@ along with atlaskit.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright
 ----
-2015 California Institute of Technology.
+2020 California Institute of Technology.
 """
 
 __version__ = '0.1.0'
@@ -38,45 +38,47 @@ import numpy as np
 
 
 def main():
-    
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Probabilistic label volumes in microliters')
-    parser.add_argument('prob_files', type=str, nargs='+', help="List of 4D prob label images")
+    parser = argparse.ArgumentParser(description='Probability weighted metric averages')
+    parser.add_argument('-p', '--probatlas', required=True, help="4D probabilistic atlas labels")
+    parser.add_argument('-m', '--metrics', required=True, nargs='+', help="Space separated list of metric images")
 
     # Parse command line arguments
     args = parser.parse_args()
-    prob_files = args.prob_files
+    probatlas_fname = args.probatlas
+    metric_fname_list = args.metrics
 
-    for p_file in prob_files:
-        
+    # Load prob atlas
+    try:
+        probatlas_nii = nib.load(probatlas_fname)
+        p = probatlas_nii.get_fdata()
+    except:
+        print('* Problem loading {} - exiting'.format(probatlas_fname))
+        raise ()
+
+    n_labels = p.shape[3]
+
+    for metric_fname in metric_fname_list:
+
         # Force absolute path
-        p_file = os.path.abspath(p_file)
-        
-        # Load the source atlas image
-        p_nii = nib.load(p_file)
-        p = p_nii.get_data()
-        nd = p.ndim
+        metric_fname = os.path.abspath(metric_fname)
 
         # Atlas voxel volume in mm^3 (microliters)
         atlas_vox_vol_ul = np.array(p_nii.header.get_zooms()).prod()
-        
+
         # Treat probabilities as partial volumes and integrate
         if nd == 3:
 
             V = np.sum(p) * atlas_vox_vol_ul
-            print('%0.3f' % V) 
+            print('%0.3f' % V)
 
         elif nd == 4:
 
-            nx,ny,nz,nt = p.shape
+            nx, ny, nz, nt = p.shape
 
-            for t in range(0,nt):
-
-                V = np.sum(p[:,:,:,t]) * atlas_vox_vol_ul
+            for t in range(0, nt):
+                V = np.sum(p[:, :, :, t]) * atlas_vox_vol_ul
                 print('%0.3f' % V),
-
-            # Final newline
-            print
 
     # Clean exit
     sys.exit(0)
