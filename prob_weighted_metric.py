1abda3253cef4fc4e31c026d957c493159b549ae
Mike Tyszka <jmt@caltech.edu>
2020-11-08 18:15:28 -0800

    Added probability-weighted mean metric function

--- prob_weighted_metric.py
+++ prob_weighted_metric.py
@@ -30,55 +30,89 @@ Copyright
 
 __version__ = '0.1.0'
 
-import os
 import sys
 import argparse
 import nibabel as nib
 import numpy as np
+import pandas as pd
+from glob import glob
 
 
 def main():
+
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Probability weighted metric averages')
     parser.add_argument('-p', '--probatlas', required=True, help="4D probabilistic atlas labels")
-    parser.add_argument('-m', '--metrics', required=True, nargs='+', help="Space separated list of metric images")
+    parser.add_argument('-m', '--metrics', required=True, nargs='+', help="Metric image(s) (wildcards supported)")
 
     # Parse command line arguments
     args = parser.parse_args()
     probatlas_fname = args.probatlas
-    metric_fname_list = args.metrics
+
+    # Compile metric filename list
+    metric_fname_list = []
+    for chunk in args.metrics:
+        metric_fname_list += glob(chunk)
 
     # Load prob atlas
     try:
+        print('Loading probabilistic atlas {}'.format(probatlas_fname))
         probatlas_nii = nib.load(probatlas_fname)
-        p = probatlas_nii.get_fdata()
-    except:
+        p_all = probatlas_nii.get_fdata()
+    except IOError as err:
         print('* Problem loading {} - exiting'.format(probatlas_fname))
-        raise ()
+        raise (err)
 
-    n_labels = p.shape[3]
+    # Number of atlas labels
+    n_labels = p_all.shape[3]
+    print('Atlas contains {} labels'.format(n_labels))
 
-    for metric_fname in metric_fname_list:
+    # Atlas voxel volume in mm^3 (microliters)
+    vox_vol_um = np.array(probatlas_nii.header.get_zooms()).prod()
+    print('Atlas voxel volume is {:0.3f} ul'.format(vox_vol_um))
 
-        # Force absolute path
-        metric_fname = os.path.abspath(metric_fname)
+    # Init stats list
+    stats = []
 
-        # Atlas voxel volume in mm^3 (microliters)
-        atlas_vox_vol_ul = np.array(p_nii.header.get_zooms()).prod()
+    # Loop over prob labels
+    for lc in range(n_labels):
 
-        # Treat probabilities as partial volumes and integrate
-        if nd == 3:
+        print()
+        print('Label {}'.format(lc))
 
-            V = np.sum(p) * atlas_vox_vol_ul
-            print('%0.3f' % V)
+        p = p_all[:, :, :, lc]
 
-        elif nd == 4:
+        # Integral of p over image space
+        p_tot = np.sum(p)
 
-            nx, ny, nz, nt = p.shape
+        for mc, metric_fname in enumerate(metric_fname_list):
 
-            for t in range(0, nt):
-                V = np.sum(p[:, :, :, t]) * atlas_vox_vol_ul
-                print('%0.3f' % V),
+            print('  {}'.format(metric_fname))
+
+            # Load this metric image
+            try:
+                metric_nii = nib.load(metric_fname)
+                m = metric_nii.get_fdata()
+            except IOError as err:
+                print('* Problem loading {} - exiting'.format(metric_fname))
+                raise(err)
+
+            # Probability-weighted mean metric for current label
+            mp = np.sum(m * p) / p_tot
+            label_vol = p_tot * vox_vol_um
+
+            stats.append([lc, label_vol, metric_fname, mp])
+
+    # Convert stats list to dataframe
+    df = pd.DataFrame(stats, columns=['Label', 'ProbLabelVolume_ul', 'MetricImageFile', 'ProbWeightedMeanMetric'])
+
+    # Export to CSV
+    csv_fname = 'prob_weighted_metrics.csv'
+
+    print()
+    print('Saving results to {}'.format(csv_fname))
+
+    df.to_csv(csv_fname, index=False)
 
     # Clean exit
     sys.exit(0)
